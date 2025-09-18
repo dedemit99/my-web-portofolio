8748a9981187c27ebb381bf0d77d88bfa8bbb5e1
Rizki Kurnia <rizkikurniasan12@gmail.com>
2025-09-18 15:41:39 +0700

    feat: navbar and styling for background and navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#project">Project</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
